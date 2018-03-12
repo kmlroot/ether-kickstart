a993f4e6c3eb4bf4b1a3666a9fe680d433195ca3
Mauricio Serna Florez <mauricioserf@gmail.com>
2018-03-11 21:15:06 -0500

    [Add] nextjs

---
+++ pages/newcampaign.js
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export default () => <h1> This is the new campaign page. </h1>
---
+++ pages/show.js
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export default () => <h1> Welcome to the show page. </h1>
