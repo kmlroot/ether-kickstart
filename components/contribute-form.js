b1e728e1f73ce2e65cc4dd0cff5336fa8cc31561
Mauricio Serna Florez <mauricioserf@gmail.com>
2018-03-18 17:11:03 -0500

    [Add] error handling to contribute form.

--- components/contribute-form.js
+++ components/contribute-form.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import { Form, Input, Button, Message } from 'semantic-ui-react'
 
+import { Router } from '../routes'
+
 import Campaign from '../eth/campaign'
 import web3 from '../eth/web3'
 
 class ContributeForm extends Component {
   state = {
-    value: ''
+    value: '',
+    errorMessage: '',
+    loading: false
   }
 
   onSubmit = async (event) => {
@@ -18,6 +22,8 @@ class ContributeForm extends Component {
 
     let accounts
 
+    this.setState({ loading: true, errorMessage: '' })
+
     try {
       accounts = await web3.eth.getAccounts()
 
@@ -25,26 +31,30 @@ class ContributeForm extends Component {
         from: accounts[0],
         value: web3.utils.toWei(value, 'ether')
       })
-    } catch (err) {}
+
+      Router.replaceRoute(`/campaigns/${address}`)
+    } catch (err) {
+      this.setState({ errorMessage: err.message })
+    }
+
+    this.setState({ loading: false, value: '' })
   }
 
   render () {
-    return (
-      <Form onSubmit={this.onSubmit}>
+    return <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
         <Form.Field>
           <label> Amout to Contribute </label>
-          <Input
-            value={this.state.value}
-            onChange={event => this.setState({ value: event.target.value })}
-            label='ether'
-            labelPosition='right'
-          />
+          <Input value={this.state.value} onChange={(event) => this.setState(
+                { value: event.target.value }
+              )} label="ether" labelPosition="right" />
         </Form.Field>
-        <Button primary>
+
+        <Message error header="Oops!" content={this.state.errorMessage} />
+
+        <Button primary loading={this.state.loading}>
           Contribute
         </Button>
       </Form>
-    )
   }
 }
 
