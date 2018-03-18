6225d188dc656ce2b0ea296db9eeefe87f863f75
Mauricio Serna Florez <mauricioserf@gmail.com>
2018-03-18 15:39:41 -0500

    [Add] next-routes and server.js

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react'
-import { Form, Button, Input } from 'semantic-ui-react'
+
+import {
+  Form,
+  Button,
+  Input,
+  Message
+} from 'semantic-ui-react'
 
 import Layout from '../../components/layout'
 
+import factory from '../../eth/factory'
+import web3 from '../../eth/web3'
+
 class CampaignNew extends Component {
   state = {
-    minimumContribution: ''
+    minimumContribution: '',
+    errorMessage: ''
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefault()
+
+    this.setState({ loading: true })
+
+    const { minimumContribution } = this.state
+
+    let accounts
+
+    try {
+      accounts = await web3.eth.getAccounts()
+
+      await factory.methods.createCampaign(minimumContribution).send({
+        from: accounts[0]
+      })
+    } catch (err) {
+      this.setState({
+        errorMessage: err.message
+      })
+    }
+
+    this.setState({ loading: false })
   }
 
   render () {
@@ -13,13 +47,20 @@ class CampaignNew extends Component {
       <Layout>
         <h3> Create a Campaign </h3>
 
-        <Form>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage} >
           <Form.Field>
             <label> Minimum contribution </label>
-            <Input label='wei' labelPosition='right' />
+            <Input
+              label='wei'
+              labelPosition='right'
+              value={this.state.minimumContribution}
+              onChange={event => this.setState({ minimumContribution: event.target.value })}
+            />
           </Form.Field>
 
-          <Button primary> Create </Button>
+          <Message error header="Oops!" content={this.state.errorMessage} />
+
+          <Button loading={this.state.loading} primary> Create </Button>
         </Form>
       </Layout>
     )
