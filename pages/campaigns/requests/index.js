d9dae8b167eea32d5a12c4d3c98fd9a20a24ad28
Mauricio Serna Florez <mauricioserf@gmail.com>
2018-03-19 11:16:44 -0500

    [Add] campaign request table

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import { Button } from 'semantic-ui-react'
+import { Button, Table } from 'semantic-ui-react'
 
 import { Link } from '../../../routes'
 import Layout from '../../../components/layout'
+import RequestRow from '../../../components/request-row'
 
 import Campaign from '../../../eth/campaign'
 
@@ -21,7 +22,20 @@ class RequestIndex extends Component {
     return { address, requests, requestCount }
   }
 
+  renderRows () {
+    return this.props.requests.map((request, index) => {
+      return <RequestRow
+        id={index}
+        key={index}
+        request={request}
+        address={this.props.address}
+      />
+    })
+  }
+
   render () {
+    const { Header, Row, HeaderCell, Body } = Table
+
     return (
       <Layout>
         <h3> Requests </h3>
@@ -31,6 +45,24 @@ class RequestIndex extends Component {
             <Button primary> Add Requests </Button>
           </a>
         </Link>
+
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+
+          <Body>
+            {this.renderRows()}
+          </Body>
+        </Table>
       </Layout>
     )
   }
